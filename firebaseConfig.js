ba18fcec5487305a93f32bf5dd89ac5d2a951f26
“QueenLaurenDivivar-D” <Joshuadeguzman1817@gmail.com>
2025-08-28 11:43:02 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWVFF4OjnzdrQaU6iaNniwDcPD0UAPEQ8",
+  authDomain: "qld-goal-tracker.firebaseapp.com",
+  projectId: "qld-goal-tracker",
+  storageBucket: "qld-goal-tracker.firebasestorage.app",
+  messagingSenderId: "686164709507",
+  appId: "1:686164709507:web:64a23c6888b2ee53ca0bb6"
+};
+
+const app = initializeApp(firebaseConfig);
