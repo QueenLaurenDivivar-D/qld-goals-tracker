85abab89f9c30af62b5491d177b171f3826408bd
Ninocabanero <cabaneronino50@gmail.com>
2025-09-02 11:05:15 +0800

    firebase DB Connection

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDWVFF4OjnzdrQaU6iaNniwDcPD0UAPEQ8",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
